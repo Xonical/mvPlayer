b6dafa5fea25acd4300b6574488cfd3e5e87edee
Sebastian Schoen <sebastian-schoen@outlook.com>
2014-08-13 17:16:00 +0200

    Create an Imagebox (Label + ImageView)

--- src/main/java/de/xonical/mvplayer/presentation/main/MainViewPresenter.java
+++ src/main/java/de/xonical/mvplayer/presentation/main/MainViewPresenter.java
@@ -1,6 +1,8 @@
 package de.xonical.mvplayer.presentation.main;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,12 +15,19 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import de.xonical.mvplayer.model.Directory;
 import de.xonical.mvplayer.model.RegistrationService;
@@ -28,31 +37,82 @@ import de.xonical.mvplayer.presentation.directories.DirectoryView;
 import de.xonical.mvplayer.presentation.directories.DirectoryPresenter;
 import de.xonical.mvplayer.util.Settings;
 
+//http://stackoverflow.com/questions/11180884/how-to-populate-a-tableview-that-is-defined-in-an-fxml-file-that-is-designed-in
+
 public class MainViewPresenter implements Initializable {
 
+	@FXML TableView<Directory> tableView;
+	@FXML private TableColumn<Directory, String> directoryColumn;
+	@FXML private TableColumn<Directory, Number> countedFilesColumn;
+
+	@FXML ImageView imageView;
+
+	@Inject RegistrationService service;
+
 	@FXML
-	AnchorPane directoriesPane;
-
-	@Inject
-	RegistrationService service;
-
-	private DirectoryPresenter directoryPresenter;
-	private DirectoryView directoryView;
+	AnchorPane anchorPane = new AnchorPane();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+		List<Directory> allDirectories = service.allDirectories();
+		ObservableList<Directory> oListDirectories = FXCollections
+				.observableList(allDirectories);
 
+		directoryColumn
+				.setCellValueFactory(new PropertyValueFactory<Directory, String>(
+						"directoryName"));
+		countedFilesColumn
+				.setCellValueFactory(new PropertyValueFactory<Directory, Number>(
+						"countedVideoFiles"));
 
-		this.directoryView = new DirectoryView();
-		directoryPresenter = (DirectoryPresenter) this.directoryView
-				.getPresenter();
+		tableView.getItems().setAll(oListDirectories);
+		tableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-		VBox vBox = new VBox();
-		vBox.getChildren().add(this.directoryView.getView());
-		this.directoriesPane.getChildren().add(vBox);
+		// final Label selected = new Label();
+		tableView.getSelectionModel().selectedItemProperty()
+				.addListener((observable, oldValue, newValue) -> {
+					System.out.println(newValue.getDirectoryName());
+
+					readImageData(newValue);
+
+				});
 	}
 
+	private void readImageData(Directory newValue) {
+		List<VideoFile> videoFiles = newValue.getVideoFiles();
+
+		for (VideoFile video : videoFiles) {
+			String thumbnailFile = video.getThumbnailFile();
+
+			System.out.println(thumbnailFile);
+
+
+                BufferedImage bufferedImage;
+				try {
+					bufferedImage = ImageIO.read(new File(thumbnailFile));
+					Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+					ImageboxView buildImageboxes = buildImageboxes(image);
+
+					//this.anchorPane.
+
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			break;
+		}
+	}
+
+	private ImageboxView buildImageboxes(Image image) {
+		ImageboxView box = new ImageboxView();
+		ImageboxPresenter presenter = (ImageboxPresenter) box.getPresenter();
+		presenter.setTextImageLabel("");
+		presenter.setImage(image);
+		return box;
+	}
 
 	private List<SubDirectory> createListOfSubDirectories(
 			ObservableList<ObservableList<VideoFile>> allVideosInRootDirectory) {
@@ -66,7 +126,8 @@ public class MainViewPresenter implements Initializable {
 
 			// Every video file of each subfolder
 			for (final VideoFile videoFile : videoFiles) {
-				final Path parent = Paths.get(new File(videoFile.getVideoFileName()).toURI());
+				final Path parent = Paths.get(new File(videoFile
+						.getVideoFileName()).toURI());
 				subDirectory = new SubDirectory(parent);
 				// we can break the loop, because we have the parent directory
 				break;
@@ -79,3 +140,24 @@ public class MainViewPresenter implements Initializable {
 		return allSubDirectories;
 	}
 }
+
+// private TableView<TableRow> myTable;
+// @FXML
+// private TableColumn<TableRow, Object> myTableColumnOne;
+// @FXML
+// private TableColumn<TableRow, Object> myTableColumnTwo;
+//
+// public void populateMyTable()
+// {
+// myTableColumnOne.setCellValueFactory(new PropertyValueFactory<TableRow,
+// Object>("one"));
+// myTableColumnTwo.setCellValueFactory(new PropertyValueFactory<TableRow,
+// Object>("two"));
+//
+// ObservableList data = FXCollections.observableArrayList();
+// for(Map.Entry<String, Integer> entry: myHashMap.entrySet())
+// {
+// data.add(new TableRow(entry.getKey(), entry.getValue()));
+// }
+// myTable.getItems().setAll(data);
+// }
